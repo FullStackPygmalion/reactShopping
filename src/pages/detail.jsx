a13b136c901069266bfd2828333cd12ec297ec3d
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-04 22:39:49 -0500

    Conexion a API original, usando hooks

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Header from '../components/header'
 import Product from '../components/product'
 
-const Detail = (props) => {
-    const product = {}
-    return (
-        <React.Fragment>
-            <Header />
-            <div className="container mt-4">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <Product {...product} />
+const API = 'https://shopping-cart-api.khriztianmoreno.now.sh/api/products'
 
-                    </div>
-                </div>
-            </div>
-            </React.Fragment>
-)}
+const getProducts = async (id, setProduct) => {
+  try {
+    const result = await fetch(API + '/' + id)
+    const product = await result.json()
+    setProduct(product)
+  } catch (error) {
+    console.error('Could not fetch products ;(', error)
+  }
+}
+
+
+const Detail = (props) => {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    getProducts(props.match.params.id, setProduct)
+  }
+    , [props])
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-lg-12">
+            <Product {...product} />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
 
 export default Detail
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Header from '../components/header'
 import Product from '../components/product'
 
-const Home = () => (
-    <React.Fragment>
-        <Header />
-        <Product name="Chocorramo" price="2500" desciption="Rico chocorramo" />
-    </React.Fragment>
-)
+const API = 'https://shopping-cart-api.khriztianmoreno.now.sh/api/products'
+
+const getProducts = async (setProducts) => {
+    try {
+        const results = await fetch(API)
+        const products = await results.json()
+        setProducts(products)
+    } catch (error) {
+        console.error('Could not fetch products ;(', error)
+    }
+}
+
+const Home = () => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        getProducts(setProducts)
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Header />
+            <div className="container mt-4">
+                <div className="row">
+                    {products.map(product => <Product key={product._id} {...product} />)}
+                </div>
+            </div>
+
+        </React.Fragment>
+    )
+}
 
 export default Home
