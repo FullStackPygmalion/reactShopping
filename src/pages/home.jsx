6d7e3ed60f463d8c4ca89978fb2d897b1bd87b24
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-06 18:06:02 -0500

    Connect register page to API

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,32 +6,32 @@ import Product from '../components/product'
 const API = 'https://shopping-cart-api.khriztianmoreno.now.sh/api/products'
 
 const getProducts = async (setProducts) => {
-    try {
-        const results = await fetch(API)
-        const products = await results.json()
-        setProducts(products)
-    } catch (error) {
-        console.error('Could not fetch products ;(', error)
-    }
+  try {
+    const results = await fetch(API)
+    const products = await results.json()
+    setProducts(products)
+  } catch (error) {
+    console.error('Could not fetch products ;(', error)
+  }
 }
 
 const Home = () => {
-    const [products, setProducts] = useState([])
-    useEffect(() => {
-        getProducts(setProducts)
-    }, []);
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    getProducts(setProducts)
+  }, []);
 
-    return (
-        <React.Fragment>
-            <Header />
-            <div className="container mt-4">
-                <div className="row">
-                    {products.map(product => <Product key={product._id} {...product} />)}
-                </div>
-            </div>
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="container mt-4">
+        <div className="row">
+          {products.map(product => <Product key={product._id} {...product} />)}
+        </div>
+      </div>
 
-        </React.Fragment>
-    )
+    </React.Fragment>
+  )
 }
 
 export default Home
